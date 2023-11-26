eb05335bd4058788797aed9987b1238defd42303
Takshan <mrbrahma.rahul@gmail.com>
2023-11-26 17:13:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -211,11 +211,12 @@ with open("README.md", "r", encoding="utf-8") as readme_file:
 
 setup(
     name="pyqsar3",
-    version="1.4.1",
+    version="1.4.2",
     description="SSU Lab Package for PYQSAR",
     author="SSU@Chemoinformatic Lab",
     author_email="chokh@ssu.ac.kr",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/Chemoinfomatics/pyqsar3",
     packages=find_packages(),
 )
